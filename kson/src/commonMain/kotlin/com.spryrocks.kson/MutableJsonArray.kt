2eb1aecade931c21a1ef9a1cfffe7eff1023231f
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-07-05 19:33:30 +0200

    Improve JsonObject and JsonArray

--- kson/src/commonMain/kotlin/com.spryrocks.kson/MutableJsonArray.kt
+++ kson/src/commonMain/kotlin/com.spryrocks.kson/MutableJsonArray.kt
@@ -1,7 +1,17 @@
 package com.spryrocks.kson
 
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.encodeToJsonElement
 
-class MutableJsonArray internal constructor(override val list: MutableList<JsonElement>): JsonArray(list) {
-    constructor(): this(mutableListOf())
+class MutableJsonArray internal constructor(override val list: MutableList<JsonElement>) : JsonArray(list) {
+    constructor() : this(mutableListOf())
+
+    fun put(value: Int) = putValueInternal(value)
+
+    fun put(value: JsonObject) = putValueInternal(value.map)
+
+    private inline fun <reified T> putValueInternal(value: T) = putInternal(Json.encodeToJsonElement(value))
+
+    private fun putInternal(value: JsonElement) = apply { list += value }
 }
