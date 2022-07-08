a97bf687cd8b34a793fd6c1be2852e226f02655e
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-07-08 15:14:12 +0200

    Improve success function

--- core/src/commonMain/kotlin/com/ionic/plugin/core/actions/BaseActionExtensions.kt
+++ core/src/commonMain/kotlin/com/ionic/plugin/core/actions/BaseActionExtensions.kt
@@ -2,5 +2,5 @@ package com.ionic.plugin.core.actions
 
 import com.spryrocks.kson.JsonObject
 
-inline fun <TDelegate : Delegate, reified TData> BaseAction<TDelegate>.success(data: TData) =
-    this.success(JsonObject.fromObject(data))
+inline fun <TDelegate : Delegate, reified TData> BaseAction<TDelegate>.success(data: TData, finish: Boolean = true) =
+    this.success(JsonObject.fromObject(data), finish)
