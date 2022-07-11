a3f6e8ee7dd970093c4a32723ce4b1e2272ee3d9
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-07-11 22:27:48 +0200

    Refactoring

--- core/src/jsMain/kotlin/com/ionic/plugin/core/actions/CallContext.kt
+++ core/src/jsMain/kotlin/com/ionic/plugin/core/actions/CallContext.kt
@@ -5,12 +5,19 @@ import com.spryrocks.kson.JsonObject
 
 @JsExport
 actual external interface CallContext {
-    actual fun getString(key: String): String?
-    actual fun getInt(key: String): Int?
-    actual fun getJsonObject(key: String): JsonObject?
-    actual fun getBoolean(key: String): Boolean?
-    actual fun getDouble(key: String): Double?
-    actual fun getJsonArray(key: String): JsonArray?
+    actual fun getString(key: String): String
+    actual fun getInt(key: String): Int
+    actual fun getJsonObject(key: String): JsonObject
+    actual fun getBoolean(key: String): Boolean
+    actual fun getDouble(key: String): Double
+    actual fun getJsonArray(key: String): JsonArray
+
+    actual fun optString(key: String): String?
+    actual fun optInt(key: String): Int?
+    actual fun optJsonObject(key: String): JsonObject?
+    actual fun optBoolean(key: String): Boolean?
+    actual fun optDouble(key: String): Double?
+    actual fun optJsonArray(key: String): JsonArray?
 
     actual fun result(result: CallContextResult, finish: Boolean)
 }
