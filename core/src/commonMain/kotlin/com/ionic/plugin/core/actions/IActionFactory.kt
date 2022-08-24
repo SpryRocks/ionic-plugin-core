c5e9cc635a2438cf05ade16ebf5341ebb492ff6c
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-08-24 23:09:32 +0200

    Add IActionFactory

--- core/src/commonMain/kotlin/com/ionic/plugin/core/actions/IActionFactory.kt
+++ core/src/commonMain/kotlin/com/ionic/plugin/core/actions/IActionFactory.kt
@@ -3,3 +3,7 @@ package com.ionic.plugin.core.actions
 interface IActionCreator<TDelegate : Delegate, TAction : BaseAction<TDelegate>> {
     fun createAction(call: CallContext): TAction
 }
+
+interface IActionFactory<TActions, TDelegate : Delegate, TAction : BaseAction<TDelegate>>  {
+    fun createAction(action: TActions, call: CallContext): TAction
+}
