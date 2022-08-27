4f50ec589ecfad6499ab9f0af1291baf0cbc28c0
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2022-08-27 21:34:16 +0200

    Add put number func to JsonObject

--- kson/src/commonMain/kotlin/com.spryrocks.kson/MutableJsonObject.kt
+++ kson/src/commonMain/kotlin/com.spryrocks.kson/MutableJsonObject.kt
@@ -9,7 +9,20 @@ class MutableJsonObject internal constructor(override val map: MutableMap<String
 
     fun put(key: String, value: String) = putValueInternal(key, value)
 
+    fun put(key: String, value: Number)  {
+        when(value) {
+            is Int -> put(key, value)
+            is Float -> put(key, value)
+            is Long -> put(key, value)
+            else -> throw NotImplementedError("unknown type")
+        }
+    }
+
     fun put(key: String, value: Int) = putValueInternal(key, value)
+
+    fun put(key: String, value: Float) = putValueInternal(key, value)
+
+    fun put(key: String, value: Long) = putValueInternal(key, value)
 
     fun put(key: String, value: Boolean) = putValueInternal(key, value)
 
