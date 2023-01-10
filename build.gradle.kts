11cef72cd4e7cff9e65271c8da214d027ff64ed2
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-01-10 18:03:57 +0100

    Refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,5 +21,5 @@ allprojects {
 
 extra.apply {
     set("mavenGroup", "com.github.SpryRocks.ionic-plugin-core")
-    set("mavenVersion", "0.0.4")
+    set("mavenVersion", "0.0.4-fix1")
 }
--- core/src/commonMain/kotlin/com/ionic/plugin/core/actions/BaseActionExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.ionic.plugin.core.actions
-
-import com.spryrocks.kson.JsonObject
-
-inline fun <TDelegate : Delegate<TMappers>, reified TData, TMappers : Mappers>
-        BaseAction<TDelegate, TMappers>.success(data: TData, finish: Boolean = true) =
-    this.success(JsonObject.fromObject(data), finish)
