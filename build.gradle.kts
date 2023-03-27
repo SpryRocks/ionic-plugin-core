2403e057672a0544f419c8af74a7445afdb4cdce
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-03-27 23:06:22 +0200

    Update kotlin to version 1.8.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
-
-        val kotlinVersion = "1.8.10"
+        val kotlinVersion = "1.8.20-RC2"
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
+
+        classpath("com.android.tools.build:gradle:7.2.2")
     }
 }
 
@@ -21,5 +21,5 @@ allprojects {
 
 extra.apply {
     set("mavenGroup", "com.github.SpryRocks.ionic-plugin-core")
-    set("mavenVersion", "0.0.7-alpha.1")
+    set("mavenVersion", "0.0.7-alpha.2")
 }
