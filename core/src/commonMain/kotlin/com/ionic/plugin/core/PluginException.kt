c3e8e58349489832f524b693367f1664462e50e2
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-06-09 20:31:10 +0200

    Add new exception

--- core/src/commonMain/kotlin/com/ionic/plugin/core/PluginException.kt
+++ core/src/commonMain/kotlin/com/ionic/plugin/core/PluginException.kt
@@ -2,9 +2,21 @@ package com.ionic.plugin.core
 
 import kotlin.js.JsExport
 
+open class PluginExceptionBase(
+    message: String? = null,
+    cause: Throwable? = null,
+) : RuntimeException(message, cause)
+
 @JsExport
 open class PluginException(
     message: String? = null,
     val code: Int? = null,
     cause: Throwable? = null,
-) : RuntimeException(message, cause)
+) : PluginExceptionBase()
+
+@JsExport
+open class PluginExceptionNew(
+    message: String? = null,
+    val code: String? = null,
+    cause: Throwable? = null,
+) : PluginExceptionBase()
--- core/src/commonMain/kotlin/com/ionic/plugin/core/actions/Mappers.kt
+++ core/src/commonMain/kotlin/com/ionic/plugin/core/actions/Mappers.kt
@@ -1,6 +1,8 @@
 package com.ionic.plugin.core.actions
 
 import com.ionic.plugin.core.PluginException
+import com.ionic.plugin.core.PluginExceptionBase
+import com.ionic.plugin.core.PluginExceptionNew
 import com.spryrocks.kson.JsonObject
 import com.spryrocks.kson.MutableJsonObject
 import kotlin.js.JsExport
@@ -20,17 +22,20 @@ abstract class Mappers {
 
 @JsExport
 open class ErrorMapper {
-    open fun map(error: Throwable): PluginException {
-        if (error is PluginException) return error
+    open fun map(error: Throwable): PluginExceptionBase {
+        if (error is PluginExceptionBase) return error
         return PluginException(error.message, cause = error)
     }
 
     open fun mapToJson(error: Throwable): JsonObject? {
-        if (error !is PluginException) return null
-
         return MutableJsonObject().apply {
             error.message?.let { put("message", it) }
-            error.code?.let { put("code", it) }
+            if (error is PluginException) {
+                error.code?.let { put("code", it) }
+            }
+            if (error is PluginExceptionNew) {
+                error.code?.let { put("code", it) }
+            }
         }
     }
 
