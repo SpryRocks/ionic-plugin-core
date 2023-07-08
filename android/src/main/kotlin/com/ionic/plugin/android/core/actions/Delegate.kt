b0291eb806ab22e20f6b528b767955da0d02fde8
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-07-08 14:50:44 +0200

    Fix android delegate

--- android/src/main/kotlin/com/ionic/plugin/android/core/actions/Delegate.kt
+++ android/src/main/kotlin/com/ionic/plugin/android/core/actions/Delegate.kt
@@ -1,12 +1,13 @@
 package com.ionic.plugin.android.core.actions
 
-import com.ionic.plugin.android.core.WrapperDelegate
+import com.ionic.plugin.android.core.utils.IActivityProvider
 import com.ionic.plugin.android.core.utils.IContextProvider
 import com.ionic.plugin.core.actions.Mappers
 import java.util.concurrent.ExecutorService
 
 interface Delegate<TMappers : Mappers>
     : com.ionic.plugin.core.actions.Delegate<TMappers>,
-    WrapperDelegate, IContextProvider {
+    IContextProvider,
+    IActivityProvider {
     val threadPool: ExecutorService
 }
