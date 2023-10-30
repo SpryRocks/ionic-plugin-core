3368fc2d18d1887c7d50518559ed02d9f3cef495
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-10-30 18:10:04 +0100

    Logs refactoring

--- core/src/commonMain/kotlin/com/ionic/plugin/core/actions/Mappers.kt
+++ core/src/commonMain/kotlin/com/ionic/plugin/core/actions/Mappers.kt
@@ -3,9 +3,10 @@ package com.ionic.plugin.core.actions
 import com.ionic.plugin.core.PluginException
 import com.ionic.plugin.core.PluginExceptionBase
 import com.ionic.plugin.core.PluginExceptionNew
-import com.spryrocks.kson.JsonObject
-import com.spryrocks.kson.MutableJsonObject
+import com.ionic.plugin.core.logger.LogParam
+import com.spryrocks.kson.*
 import kotlin.js.JsExport
+import kotlin.reflect.KClass
 
 @JsExport
 abstract class Mappers {
@@ -18,6 +19,7 @@ abstract class Mappers {
     }
 
     abstract val errorMapper: ErrorMapper
+    open val logMapper: LogMapper = LogMapper()
 }
 
 @JsExport
@@ -41,5 +43,67 @@ open class ErrorMapper {
 
     open fun reportError(error: Throwable?, callContext: CallContext, finish: Boolean) {
         callContext.error(error, finish)
+    }
+}
+
+typealias LogMapperObjectFormatter<T> = (obj: T) -> JsonObject
+
+open class LogMapper {
+    private val formatters = mutableMapOf<KClass<*>, LogMapperObjectFormatter<*>>()
+
+    protected fun <T: Any> register(objectClass: KClass<T>, formatter: LogMapperObjectFormatter<T>) {
+        formatters[objectClass] = formatter
+    }
+
+    private fun mapLogObject(obj: Any): JsonObject? {
+        val formatter = formatters[obj::class]
+        return if (formatter != null) {
+            (formatter as LogMapperObjectFormatter<Any>)(obj)
+        } else {
+            null
+        }
+    }
+
+    internal fun formatParams(params: Array<out LogParam>): JsonObject {
+        return mutableJsonObject().apply {
+            params.forEach {
+                val value = formatValue(it.second)
+                if (value != null) {
+                    put(it.first, value)
+                } else {
+                    putNull(it.first)
+                }
+            }
+        }
+    }
+
+    private fun formatValue(value: Any?): JsonValue? {
+        if (value == null) return null
+        if (value is Array<*>) {
+            return formatArray(value)
+        }
+        if (value is String || value is Number || value is Boolean) {
+            return value
+        }
+        return formatObject(value)
+    }
+
+    private fun formatArray(array: Array<*>): JsonArray {
+        return mutableJsonArray().apply {
+            array.forEach {
+                val value = formatValue(it)
+                if (value != null) {
+                    put(value)
+                } else {
+                    putNull()
+                }
+            }
+        }
+    }
+
+    private fun formatObject(obj: Any): JsonValue {
+        val formatted = mapLogObject(obj)
+        if (formatted != null) return formatted
+        return obj.toString()
     }
 }
