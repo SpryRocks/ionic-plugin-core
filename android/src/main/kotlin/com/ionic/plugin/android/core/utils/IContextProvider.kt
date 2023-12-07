b024a793bddd544d54fc1fed084c9c6154315e96
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-12-07 17:28:39 +0100

    ActivityProvider and ContextProvider refactoring

--- android/src/main/kotlin/com/ionic/plugin/android/core/utils/IContextProvider.kt
+++ android/src/main/kotlin/com/ionic/plugin/android/core/utils/IContextProvider.kt
@@ -5,3 +5,8 @@ import android.content.Context
 interface IContextProvider {
     val context: Context
 }
+
+interface WithContextProvider: IContextProvider {
+    val contextProvider: IContextProvider
+    override val context get() = contextProvider.context
+}
