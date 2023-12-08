68d987c162aa1576840f99e9d2ca2de9b6b88279
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-12-08 18:16:18 +0100

    Add ApplicationProvider

--- android/src/main/kotlin/com/ionic/plugin/android/core/utils/IActivityProvider.kt
+++ android/src/main/kotlin/com/ionic/plugin/android/core/utils/IActivityProvider.kt
@@ -1,13 +1,18 @@
 package com.ionic.plugin.android.core.utils
 
 import android.app.Activity
+import android.app.Application
 
 interface IActivityProvider {
     val activity: Activity
 }
 
-interface WithActivityProvider: IActivityProvider, IContextProvider {
+interface WithActivityProvider:
+    IActivityProvider,
+    IContextProvider,
+    IApplicationProvider {
     val activityProvider: IActivityProvider
     override val activity get() = activityProvider.activity
     override val context get() = activity
+    override val application: Application get() = activity.application
 }
---
+++ android/src/main/kotlin/com/ionic/plugin/android/core/utils/IApplicationProvider.kt
@@ -0,0 +1,7 @@
+package com.ionic.plugin.android.core.utils
+
+import android.app.Application
+
+interface IApplicationProvider {
+    val application: Application
+}
